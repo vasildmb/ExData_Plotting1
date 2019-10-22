386b044d62ff8cc3c4dafeae76efc8fa3fd22b15
vasildmb <55053243+vasildmb@users.noreply.github.com>
2019-10-22 12:24:46 +0200

    Add files via upload

---
+++ Plot1.R
@@ -0,0 +1,15 @@
+df <- read.table('household_power_consumption.txt', header = T, sep = ';', skip =  , nrows = 100000)
+df_1 <- df
+df_1$Date <- as.Date(df_1$Date, '%d/%m/%Y')
+df_1[, 'Combined_time'] <- as.POSIXct(paste(df_1$Date, df_1$Time), format="%Y-%m-%d %H:%M:%S")
+df2 <- df_1[df_1$Date == '2007/02/01' ,]
+df3 <- df_1[df_1$Date == '2007/02/02' ,]
+df4 <- rbind(df2, df3)
+columns_to_select <- 3:9
+
+df4[, columns_to_select] <- sapply( X= df4[, columns_to_select], FUN = function(x) as.numeric(as.character(x)))
+
+#Plot1
+png(filename =  'plot1.png', width = 480, height = 480)
+hist(df4$Global_active_power, col = 'red', xlab = 'Global Active Power (kilowatts)', main = 'Global Active Power')
+dev.off()
---
+++ Plot4.R
@@ -0,0 +1,23 @@
+df <- read.table('household_power_consumption.txt', header = T, sep = ';', skip =  , nrows = 100000)
+df_1 <- df
+df_1$Date <- as.Date(df_1$Date, '%d/%m/%Y')
+df_1[, 'Combined_time'] <- as.POSIXct(paste(df_1$Date, df_1$Time), format="%Y-%m-%d %H:%M:%S")
+df2 <- df_1[df_1$Date == '2007/02/01' ,]
+df3 <- df_1[df_1$Date == '2007/02/02' ,]
+df4 <- rbind(df2, df3)
+columns_to_select <- 3:9
+
+df4[, columns_to_select] <- sapply( X= df4[, columns_to_select], FUN = function(x) as.numeric(as.character(x)))
+
+
+#Plot4
+png(filename =  'plot4.png', width = 480, height = 480)
+par(mfrow = c(2, 2))
+plot(df4$Combined_time, df4$Global_active_power, type = 'l' , xlab = '', main = '', ylab ='Global Active Power (kilowatts)')
+plot(df4$Combined_time, df4$Voltage, ylab = 'Voltage', xlab = 'datetime', type = 'l')
+plot(df4$Combined_time, df4$Sub_metering_1, type = 'l', ylab ='Energy sub metering', xlab = '')
+lines(df4$Combined_time, df4$Sub_metering_2, type = 'l', col = 'red')
+lines(df4$Combined_time, df4$Sub_metering_3, type = 'l', col = 'blue')
+legend('topright', col = c('black', 'red', 'blue'), legend = c('Sub_metering_1', 'Sub_metering_2', 'Sub_metering_3'), lwd   = 1, cex = 0.7, bty = 'n' )
+plot(df4$Combined_time , df4$Global_reactive_power, type = 'l', ylab = 'Global_reactive_power', xlab = 'datetime')
+dev.off()
