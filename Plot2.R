67d7d240fa9890909533c72510c0d15bbcf01f5c
vasildmb <55053243+vasildmb@users.noreply.github.com>
2019-10-25 14:43:30 +0200

    Update Plot2.R

--- Plot2.R
+++ Plot2.R
@@ -1,13 +1,13 @@
-df <- read.table('household_power_consumption.txt', header = T, sep = ';', skip =  , nrows = 100000)
-df_1 <- df
-df_1$Date <- as.Date(df_1$Date, '%d/%m/%Y')
-df_1[, 'Combined_time'] <- as.POSIXct(paste(df_1$Date, df_1$Time), format="%Y-%m-%d %H:%M:%S")
-df2 <- df_1[df_1$Date == '2007/02/01' ,]
-df3 <- df_1[df_1$Date == '2007/02/02' ,]
-df4 <- rbind(df2, df3)
-columns_to_select <- 3:9
+df <- read.table('household_power_consumption.txt', header = T, sep = ';', skip =  , nrows = 100000) #The file is available in the folder as a zip file
+df_1 <- df    #A copy of the first dataframe that will be modifyed so that I keep a copy of the original data as quality co ntrol
+df_1$Date <- as.Date(df_1$Date, '%d/%m/%Y')   #Convert the dates to a date format
+df_1[, 'Combined_time'] <- as.POSIXct(paste(df_1$Date, df_1$Time), format="%Y-%m-%d %H:%M:%S") #Convert the dates to a date format
+df2 <- df_1[df_1$Date == '2007/02/01' ,]   #Select for dates '2007/02/01' and '2007/02/02'
+df3 <- df_1[df_1$Date == '2007/02/02' ,]   #Select for dates '2007/02/01' and '2007/02/02'
+df4 <- rbind(df2, df3)                       #Select for dates '2007/02/01' and '2007/02/02'
+columns_to_select <- 3:9       #columns to be converteb in numerical
 
-df4[, columns_to_select] <- sapply( X= df4[, columns_to_select], FUN = function(x) as.numeric(as.character(x)))
+df4[, columns_to_select] <- sapply( X= df4[, columns_to_select], FUN = function(x) as.numeric(as.character(x)))   #converting in numerical
 
 
 #Plot2
